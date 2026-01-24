38dc3b242b35a10f39b803b267626de87ae45664
EliasdsAI <jedantas64@gmail.com>
2026-01-23 21:03:37 -0300

    Add files via upload

---
+++ principal/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def inicio(request):
+    return render(request, 'home.html')
+
+def historia(request):
+    return render(request, 'historia.html')
+
+def administrativo(request):
+    return render(request,'menuadm.html')
